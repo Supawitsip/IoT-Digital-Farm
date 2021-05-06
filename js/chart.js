f5b0801931e011229aafcfd1191144c9a75411d1
Supawit <supawitsip@gmail.com>
2021-05-06 15:49:53 +0700

    new

--- js/chart.js
+++ js/chart.js
@@ -7,79 +7,149 @@ dbRef.child("devices_sensor").get().then((snapshot) => {
 
     // Get Number of device connected
     num_of_devi = Object.keys(deviObj).length;
-    //console.log('All Devices:' + num_of_devi);
-    //document.getElementById('allDevices').innerText = num_of_devi + " devices";
+
 
     // Get the lastest time (of D02)
     n = Object.keys(deviObj.D02).length;
     last_element = Object.keys(deviObj.D02)[n-1];
     last_time = deviObj.D02[last_element].timeUpdate.timestamp;
    // document.getElementById('lastTime').innerText = last_time;
-    let tem_aryD02 = [];
+    
+    // Devices get data
     let tem_aryD01 = [];
-    // Get all devices info and display it
+    let tem_aryD02 = [];
+    let humi_aryD01 = [];
+    let humi_aryD02 = [];
+    let time_aryD01 = [];
+    let time_aryD02 = [];
     let i = 0;
     for (d in deviObj.D01) {
       n_sampling = Object.keys(deviObj.D01).length;
-      all_samp = Object.keys(deviObj.D01)[i]; //Object.keys(deviObj[d])[100];
-      //last_samp = Object.keys(deviObj[d])[n_sampling-1]; //Object.keys(deviObj[d])[100];
+      all_samp = Object.keys(deviObj.D01)[i];
       i += 1
       tem_aryD01.push(deviObj.D01[all_samp].temperature);
-      /*
-      let d_name = deviObj[d][last_samp].deviceNameID;
-      let humi = deviObj[d][last_samp].humidity;
-      let humi_try = deviObj[d][last_samp].humidity;
-      console.log("I " + humi_try);
-      let temp = deviObj[d][last_samp].temperature;
-      console.log(d_name + ': ' + humi + ', ' + temp);
-
-      //let devi_info = document.createElement('div');
-      //devi_info.setAttribute('class', 'devi-block');*/
+      time_aryD01.push(deviObj.D01[all_samp].timeUpdate.timestamp);
+      humi_aryD01.push(deviObj.D01[all_samp].humidity);
     }
     i = 0;
     for (d in deviObj.D02) {
-      
       n_sampling = Object.keys(deviObj.D02).length;
       all_samp = Object.keys(deviObj.D02)[i]; 
       i += 1
       tem_aryD02.push(deviObj.D02[all_samp].temperature);
+      time_aryD02.push(deviObj.D02[all_samp].timeUpdate.timestamp);
+      humi_aryD02.push(deviObj.D02[all_samp].humidity);
     }
-    //console.log(tem_ary);
-    
-      var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-          
-            type: 'line',
-            data: {
-              labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
-              datasets: [{ 
-                  data: tem_aryD01,
-                  label: "D01",
-                  borderColor: "#3e95cd",
-                  backgroundColor: "#7bb6dd",
-                  fill: false,
-                }, { 
-                  data: tem_aryD02,
-                  label: "D02",
-                  borderColor: "#3cba9f",
-                  backgroundColor: "#71d1bd",
-                  fill: false,
-                }/*, { 
-                  data: [10,21,60,44,17,21,17],
-                  label: "Pending",
-                  borderColor: "#ffa500",
-                  backgroundColor:"#ffc04d",
-                  fill: false,
-                }, { 
-                  data: [6,3,2,2,7,0,16],
-                  label: "Rejected",
-                  borderColor: "#c45850",
-                  backgroundColor:"#d78f89",
-                  fill: false,
-                }*/
-              ]
-            },
-          });
+
+    //D01
+    let temp_data30_D01 = [];
+    let date_data30_D01 = [];
+    let humi_data30_D01 = [];
+    if (tem_aryD01.length > 0 ) {
+      for(let j = 0; j <= 30; j++){
+        temp_data30_D01.push(tem_aryD01[tem_aryD01.length-30+j]);
+        date_data30_D01.push(time_aryD01[tem_aryD01.length-30+j]);
+        humi_data30_D01.push(humi_aryD01[tem_aryD01.length-30+j]);
+      }
+    }
+    //D02
+    let temp_data30_D02 = [];
+    let date_data30_D02 = [];
+    let humi_data30_D02 = [];
+    if (tem_aryD02.length > 0 ) {
+      for(let j = 0; j <= 30; j++){
+        temp_data30_D02.push(tem_aryD02[tem_aryD02.length-30+j]);
+        date_data30_D02.push(time_aryD02[tem_aryD02.length-30+j]);
+        humi_data30_D02.push(humi_aryD02[tem_aryD02.length-30+j]);
+      }
+    }
+
+    // teperature grapt
+    console.log(humi_data30_D02);
+    var ctx = document.getElementById('temperatureChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: date_data30_D02,
+        datasets: [{ 
+            data: temp_data30_D01,
+            label: "D01",
+            borderColor: "#3e95cd",
+            backgroundColor: "#7bb6dd",
+            fill: false,
+          }, { 
+            data: temp_data30_D02,
+            label: "D02",
+            borderColor: "#3cba9f",
+            backgroundColor: "#71d1bd",
+            fill: false,
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              display: true,
+              ticks: {
+                suggestedMin: 0, 
+                suggestedMax: 40,
+              }
+            }],
+            xAxes: [{
+             ticks: {
+                    display: false
+             }
+           }]
+         },
+         title: {
+          display: true,
+            text: 'Temperature Chart'
+         }
+      }
+    });
+
+
+    // humidity grapt
+    var ctx = document.getElementById('humidityChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: date_data30_D02,
+        datasets: [{ 
+            data:  humi_data30_D01,
+            label: "D01",
+            borderColor: "#3e95cd",
+            backgroundColor: "#7bb6dd",
+            fill: false,
+          }, { 
+            data:  humi_data30_D02,
+            label: "D02",
+            borderColor: "#3cba9f",
+            backgroundColor: "#71d1bd",
+            fill: false,
+          }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+              suggestedMin: 0, 
+              suggestedMax: 100,
+            }
+          }],
+          xAxes: [{
+           ticks: {
+                  display: false
+           }
+         }]
+       },
+       title: {
+        display: true,
+          text: 'Temperature Chart'
+       }
+    }
+  });
+
   } else {
     console.log("No data available");
   }
@@ -88,12 +158,5 @@ dbRef.child("devices_sensor").get().then((snapshot) => {
 });
 
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-}
 
 
