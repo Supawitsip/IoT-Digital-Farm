512cd57ff0c349e53afa0b0cf2b987876dba4a68
Supawit <supawitsip@gmail.com>
2021-05-13 18:52:56 +0700

    s

--- js/report.js
+++ js/report.js
@@ -208,9 +208,6 @@ function exportGraph2pdf() {
     pdf.save('graph-report.pdf');
 }
 
-var presenceRef = firebase.database().ref("disconnectmessage");
-// // Write a string when this client loses connection
-presenceRef.onDisconnect().set("I disconnected!");
 
 
 var label;
