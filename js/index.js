aa695103595a297c9b483868e0022cfc89cbe5bf
Supawit <supawitsip@gmail.com>
2021-05-17 17:04:40 +0700

    v

--- js/index.js
+++ js/index.js
@@ -46,7 +46,7 @@ function deviceClickHandler() {
     clicked.addEventListener('click', function() {
         let thisName = this.getAttribute("dname");
         //Save devices data to Local Storage
-        dbRef.child(db_devices_data).child(thisName).get().then((snapshot) => {
+        dbRef.child(db_devices_data).child(thisName).limitToLast(50000).get().then((snapshot) => {
           localStorage.setItem('deviceObject', JSON.stringify(snapshot.val()));
           location.href=`/report.html?device=${thisName}`;
         });
